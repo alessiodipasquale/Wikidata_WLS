6dced503cb81e6e0f2807fc3e754b37138d45c9d
Alessio Di Pasquale <38081627+alessiodipasquale@users.noreply.github.com>
2023-02-07 17:28:00 +0100

    Added count properties script

---
+++ countProperties.py
@@ -0,0 +1,34 @@
+import json
+import os
+from tqdm import tqdm
+
+properties = {}
+ 
+dir_path = "/Users/alessiodipasquale/Desktop/textual_heritage/"
+pbar = tqdm(total=len([entry for entry in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, entry))]))
+for file in os.listdir(dir_path):
+    try:     
+        with open(dir_path+file,'r') as f:
+            data = json.load(f)       
+            entities = data['entities']
+            for key in entities: #entità Q
+                el = entities[key]
+                for claimsId in el['claims']: #proprietà P
+                    statements = el['claims'][claimsId]
+                    if(claimsId not in properties.keys()): 
+                        toChange = {claimsId: len(statements)} 
+                        properties.update(toChange)
+                    else:
+                        toChange = {claimsId: properties.get(claimsId)+len(statements)} 
+                        properties.update(toChange)
+    except KeyError:
+        print('key error')
+        #with open('G:/asserted3/errors/rankingsError.txt','a') as errorFile:
+            #errorFile.write(file)
+    except json.JSONDecodeError as err:
+        print('json error')
+    pbar.update(1)
+
+    
+with open('./countProperties.json','w') as outfile:
+    outfile.write(json.dumps(properties, indent = 4))
